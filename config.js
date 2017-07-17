7c739d52fb17e8d46a99cc03386076f9dfbca290
tedinh <tedinh@gmail.com>
2017-07-17 08:46:18 -0500

    Removed .env and config

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  sessionSecret: 'keyboard cat',
-  bucketName: 'terrysblog',
-    accessKeyId: 'AKIAJL23G6QLDNUBI2TA',
-    secretAccessKey: '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',
-    region: 'us-east-2',
-    signatureVersion: 'v4'
-};
